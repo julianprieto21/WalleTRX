0cb20923056866bf402c91b0bafa842d43623980
Julian Prieto <prietojulian2003@gmail.com>
2024-03-18 19:35:56 -0300

    Rename 'account' Page to 'accounts' & refactor SideNav

--- app/components/NavLinks.tsx
+++ app/components/NavLinks.tsx
@@ -5,8 +5,8 @@ import {
   BanknotesIcon,
   ChartBarIcon,
   ArrowTrendingUpIcon,
-  UserIcon,
   Cog6ToothIcon,
+  RectangleStackIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -26,10 +26,7 @@ const mainLinks = [
     icon: ArrowTrendingUpIcon,
   },
   { name: `${lang.dashboardText}`, href: "/dashboard", icon: ChartBarIcon },
-];
-
-const secondaryLinks = [
-  { name: `${"Account"}`, href: "/account", icon: UserIcon },
+  { name: `${"Accounts"}`, href: "/accounts", icon: RectangleStackIcon },
   { name: `${"Settings"}`, href: "/settings", icon: Cog6ToothIcon },
 ];
 
@@ -54,22 +51,6 @@ export default function NavLinks({ isLogIn }: { isLogIn: boolean }) {
         );
       })}
       <HLine width={100} color="neutral" />
-      {secondaryLinks.map((link) => {
-        const LinkIcon = link.icon;
-        return (
-          <Link
-            key={link.name}
-            href={link.href}
-            className={`flex h-[3rem] grow items-center justify-start gap-2 text-neutral-400 p-4 text-md hover> font-normal hover:bg-neutral-700 hover:text-neutral-50 sm:flex-none sm:justify-center lg:justify-start md:p-2 md:px-3 
-                ${
-                  pathName === link.href ? "bg-neutral-700 text-neutral-50" : ""
-                } ${isLogIn ? "cursor-pointer" : " cursor-not-allowed"}`}
-          >
-            <LinkIcon className="w-6" />
-            <p className="block sm:hidden lg:block">{link.name}</p>
-          </Link>
-        );
-      })}
     </>
   );
 }
