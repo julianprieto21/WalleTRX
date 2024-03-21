e4f23422530c1d22e5ff121ade176c733b7b1c2b
Julian Prieto <prietojulian2003@gmail.com>
2024-03-20 23:51:04 -0300

    Add: temporal LogOut button in Settings Page

--- app/(pages)/settings/page.tsx
+++ app/(pages)/settings/page.tsx
@@ -1,5 +1,10 @@
 import { ComingSoon } from "@/app/components/ComingSoon";
+import { LogOutButton } from "@/app/components/SideNav";
 
 export default function SettingsPage() {
-  return <ComingSoon />;
+  return (
+    <main className="size-full flex justify-center items-center">
+      <LogOutButton />
+    </main>
+  );
 }
