f4110e51e4172aafc090a9a8144a133d33e3bccf
Julian Prieto <prietojulian2003@gmail.com>
2024-03-25 16:25:27 -0300

    Add: balance change percentage info

--- app/components/accounts/AccItem.tsx
+++ app/components/accounts/AccItem.tsx
@@ -2,7 +2,7 @@
 import { Account } from "@/app/lib/types";
 import { formatBalance, getBalanceByDay } from "@/app/lib/utils";
 import { ResponsiveContainer, LineChart, Line } from "recharts";
-import { FastArrowRight } from "iconoir-react";
+import { FastArrowRight, StatDown, StatUp } from "iconoir-react";
 import Link from "next/link";
 
 interface Props {
@@ -15,6 +15,11 @@ interface Props {
 }
 export default function AccountItem({ account }: Props) {
   const history = getBalanceByDay(account.history);
+  const initialBalance = history[0].balance;
+  const lastBalance = history[history.length - 1].balance;
+  const balanceChangePercentage =
+    ((lastBalance - initialBalance) / initialBalance) * 100;
+  const balanceIcon = balanceChangePercentage > 0 ? <StatUp /> : <StatDown />;
   return (
     <main className="flex flex-row items-center gap-4">
       <Link
@@ -23,15 +28,23 @@ export default function AccountItem({ account }: Props) {
         className="w-full xl:w-1/2 h-auto xl:size-auto mx-auto xl:mx-0 rounded-xl bg-neutral-50 peer cursor-pointer"
       >
         <section className="flex flex-row justify-between items-center pl-2">
-          <div className="flex flex-col h-28 w-2/5 justify-start items-center p-3">
+          <div className="flex flex-col h-32 w-2/5 justify-start items-center p-3 gap-1">
             <h1 className="w-full flex flex-row gap-2 items-center font-semibold text-normal xl:text-lg">
               {account.account.name}
             </h1>
-            <p className="w-full font-light text-neutral-500 text-md">
+            <p className="w-full flex flex-row justify-start gap-1 font-normal text-neutral-500 text-md">
               {formatBalance(account.income + account.expense)}
+              <strong className="font-medium text-neutral-500">
+                {account.account.currency}
+              </strong>
             </p>
-            <p className="w-full font-medium text-neutral-500">
-              {account.account.currency}
+            <p
+              className={`flex flex-row justify-start gap-1 w-full text-sm ${
+                balanceChangePercentage > 0 ? "text-green-500" : "text-red-500"
+              }`}
+            >
+              {balanceIcon}%{" "}
+              {balanceChangePercentage.toPrecision(4).replace("-", "")}
             </p>
           </div>
           <div className="flex w-3/5 h-28 p-3">
