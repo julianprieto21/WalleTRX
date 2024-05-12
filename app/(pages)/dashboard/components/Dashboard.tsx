3cedb047685b701a91d4cb1cc1588f0074c3b958
Julian Prieto <prietojulian2003@gmail.com>
2024-05-12 17:56:01 -0300

    add dashboard skeleton

--- app/(pages)/dashboard/components/Dashboard.tsx
+++ app/(pages)/dashboard/components/Dashboard.tsx
@@ -4,7 +4,22 @@ import CategoryChart from "./CategoryChart";
 import MonthlyChart from "./MonthlyChart";
 import TimeLine from "./TimeLine";
 
-export default async function Dashboard() {
+export function DashboardSkeleton() {
+  return (
+    <section className="size-full flex flex-row gap-10">
+      <div className="h-full w-[524px] bg-palette-300 rounded-lg shadow p-4 md:p-6"></div>
+      <div className="flex flex-col w-full gap-8">
+        <div className="w-full h-[377px] flex flex-row gap-10">
+          <div className="w-full h-full bg-palette-300 rounded-lg shadow p-4 md:p-6"></div>
+          <div className="max-w-sm w-full h-full bg-palette-300 rounded-lg shadow p-4 md:p-6"></div>
+        </div>
+        <div className="w-full h-[350px] bg-palette-300 rounded-lg shadow p-4 md:p-6"></div>
+      </div>
+    </section>
+  );
+}
+
+export async function Dashboard() {
   const monthlyChartData = await getChartData(0);
   const categoryChartDate = await getChartData(1);
   const timeLineData = await getChartData(2);
@@ -21,4 +36,6 @@ export default async function Dashboard() {
       </div>
     </section>
   );
+}
+{
 }
--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,12 +1,7 @@
 import Breadcrumbs from "@components/Breadcrumbs";
 import { dict } from "@lib/dictionaries";
-import MonthlyChart from "./components/MonthlyChart";
-import { getChartData } from "@lib/db";
-import CategoryChart from "./components/CategoryChart";
-import TimeLine from "./components/TimeLine";
-import AccountChart from "./components/AccountChart";
 import { Suspense } from "react";
-import Dashboard from "./components/Dashboard";
+import { Dashboard, DashboardSkeleton } from "./components/Dashboard";
 
 export default async function DashboardPage() {
   const { nav: text } = dict;
@@ -18,7 +13,7 @@ export default async function DashboardPage() {
           { label: text.dashboard, href: "/dashboard", active: true },
         ]}
       />
-      <Suspense fallback={<div></div>}>
+      <Suspense fallback={<DashboardSkeleton />}>
         <Dashboard />
       </Suspense>
     </main>
