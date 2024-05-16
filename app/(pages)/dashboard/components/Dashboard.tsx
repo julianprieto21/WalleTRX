76f44ff9c03b6f14aded33264a854e4d8e9bcaeb
Julian Prieto <prietojulian2003@gmail.com>
2024-05-16 19:40:25 -0300

    add Loader to dashboard page (maybe change)

--- app/(pages)/dashboard/components/Dashboard.tsx
+++ app/(pages)/dashboard/components/Dashboard.tsx
@@ -3,17 +3,26 @@ import AccountChart from "./AccountChart";
 import CategoryChart from "./CategoryChart";
 import MonthlyChart from "./MonthlyChart";
 import TimeLine from "./TimeLine";
+import Loader from "@components/Loader";
 
 export function DashboardSkeleton() {
   return (
     <section className="size-full flex flex-row gap-10">
-      <div className="h-full w-[524px] bg-palette-300 rounded-lg shadow p-4 md:p-6"></div>
+      <div className="h-full grid place-content-center w-[524px] bg-palette-300 rounded-lg shadow p-4 md:p-6">
+        <Loader />
+      </div>
       <div className="flex flex-col w-full gap-8">
         <div className="w-full h-[377px] flex flex-row gap-10">
-          <div className="w-full h-full bg-palette-300 rounded-lg shadow p-4 md:p-6"></div>
-          <div className="max-w-sm w-full h-full bg-palette-300 rounded-lg shadow p-4 md:p-6"></div>
+          <div className="w-full grid place-content-center h-full bg-palette-300 rounded-lg shadow p-4 md:p-6">
+            <Loader />
+          </div>
+          <div className="max-w-sm grid place-content-center w-full h-full bg-palette-300 rounded-lg shadow p-4 md:p-6">
+            <Loader />
+          </div>
         </div>
-        <div className="w-full h-[350px] bg-palette-300 rounded-lg shadow p-4 md:p-6"></div>
+        <div className="w-full grid place-content-center h-[350px] bg-palette-300 rounded-lg shadow p-4 md:p-6">
+          <Loader />
+        </div>
       </div>
     </section>
   );
