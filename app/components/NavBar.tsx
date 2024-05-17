11199fe9b6422b26a910eed29e4475aa80547973
Julian Prieto <prietojulian2003@gmail.com>
2024-05-17 12:23:07 -0300

    remove unused async component and refactor

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,7 +1,6 @@
 import NavLinks from "./NavLinks";
-import { auth } from "@auth";
-import { WalletSolid } from "iconoir-react";
 import {
+  WalletSolid,
   AppleWallet,
   GraphUp,
   Home,
@@ -12,21 +11,20 @@ import {
 import Link from "next/link";
 import { dict } from "@lib/dictionaries";
 
-export async function SideNav() {
+export function SideNav() {
   const { footer: footerText } = dict.nav;
-  const session = await auth();
   const title = dict.app.split("&");
   return (
-    <aside className="hidden sm:flex w-1/5 flex-none flex-col items-center justify-center bg-palette-300 h-screen sm:pb-2 overflow-auto">
+    <aside className="relative hidden sm:flex w-1/6 flex-none flex-col items-center justify-center bg-palette-300 h-screen sm:pb-2 overflow-auto">
       <section className="w-full flex flex-col items-center text-neutral-200 bg-palette-300 sm:py-4 sm:pb-2 lg:py-3 border-b border-palette-250">
-        <WalletSolid className="xl:size-36 2xl:size-48 text-palette-100" />
+        <WalletSolid className="xl:size-36 2xl:size-36 text-palette-100" />
         <h1 className="flex flex-row justify-center font-light xl:text-4xl 2xl:text-5xl text-palette-100">
           {title[0]}
           <strong className="font-semibold text-palette-500">{title[1]}</strong>
         </h1>
       </section>
       <div className="hidden sm:flex flex-col w-full h-[90%] justify-around lg:justify-start lg:gap-2 3xl:gap-4 py-4">
-        <NavLinks isLogIn={session ? true : false} />
+        <NavLinks />
       </div>
       <footer className="w-full hidden sm:flex flex-col justify-center items-center border-t border-palette-250">
         <h1 className="text-xs md:text-sm text-center font-light text-neutral-500 pt-2">
--- app/components/NavLinks.tsx
+++ app/components/NavLinks.tsx
@@ -12,46 +12,7 @@ import {
 } from "iconoir-react";
 import { dict } from "@lib/dictionaries";
 
-type LinkProps = {
-  isLogin: boolean;
-  pathName: string;
-  link: {
-    name: string;
-    href: string;
-    icon: any;
-    disabled?: boolean;
-  };
-};
-const LinkItem = ({ link, pathName, isLogin }: LinkProps) => {
-  const LinkIcon = link.icon;
-  return (
-    <>
-      <Link
-        key={link.name}
-        href={link.href}
-        className={`flex h-20 sm:h-12 grow items-center justify-start gap-2 text-palette-100 p-4 text-md hover> font-normal hover:bg-palette-250 hover:text-palette-500 sm:flex-none sm:justify-center lg:justify-start md:p-2 md:px-3 
-            ${
-              pathName === link.href ? "bg-palette-250 text-palette-500" : ""
-            } ${isLogin ? "cursor-pointer" : " cursor-not-allowed"}
-            ${link.disabled ? "pointer-events-none" : ""}`}
-      >
-        <LinkIcon className="size-10 xl:size-6 2xl:size-7" />
-        <h1 className="text-xl xl:text-sm 2xl:text-base text-palette-100 relative">
-          {link.name}
-          <p
-            className={`${
-              link.disabled ? "text-xs font-thin text-palette-500" : "hidden"
-            }`}
-          >
-            Proximamente
-          </p>
-        </h1>
-      </Link>
-    </>
-  );
-};
-
-export default function NavLinks({ isLogIn }: { isLogIn: boolean }) {
+export default function NavLinks() {
   const { nav } = dict;
   const pathName = usePathname();
   const mainLinks = [
@@ -77,14 +38,32 @@ export default function NavLinks({ isLogIn }: { isLogIn: boolean }) {
   ];
   return (
     <>
-      {mainLinks.map((link) => (
-        <LinkItem
-          key={link.name}
-          pathName={pathName}
-          isLogin={isLogIn}
-          link={link}
-        />
-      ))}
+      {mainLinks.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={`flex h-20 sm:h-12 grow items-center justify-start gap-3 text-palette-100 p-4 text-md hover> font-normal hover:bg-palette-250 hover:text-palette-500 sm:flex-none sm:justify-center lg:justify-start md:p-2 md:px-3 
+            ${pathName === link.href ? "bg-palette-250 text-palette-500" : ""}
+            ${link.disabled ? "pointer-events-none" : ""}`}
+          >
+            <LinkIcon className="size-10 xl:size-6 2xl:size-8" />
+            <h1 className="text-xl xl:text-sm 2xl:text-base text-palette-100 relative">
+              {link.name}
+              <p
+                className={`${
+                  link.disabled
+                    ? "text-xs font-thin text-palette-500"
+                    : "hidden"
+                }`}
+              >
+                Proximamente
+              </p>
+            </h1>
+          </Link>
+        );
+      })}
     </>
   );
 }
