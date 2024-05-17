fa5658a66161ad4d277d5f3f655ce1d4fcdb11c6
Julian Prieto <prietojulian2003@gmail.com>
2024-05-16 21:22:22 -0300

    add 'go to transactions' button

--- app/components/wallet/Last5Transactions.tsx
+++ app/components/wallet/Last5Transactions.tsx
@@ -1,13 +1,22 @@
 import { getTransactions } from "@lib/db";
 import { formatBalance } from "@lib/utils";
 import _ from "lodash";
+import { ArrowUpLeft } from "iconoir-react";
+import Link from "next/link";
 
 export default async function Last5transactions() {
   const transactions = await getTransactions();
   const last5 = transactions.slice(0, 5);
   return (
-    <div className="flex flex-col rounded-lg h-96 w-full mt-10 shadow-md bg-palette-300 px-6 py-5">
-      <h2 className="w-full text-right text-palette-100 text-xl font-medium">
+    <div className="flex flex-col rounded-lg h-96 w-full mt-10 shadow-md bg-palette-300 px-6 py-5 relative">
+      <Link
+        href={"/transactions"}
+        title="Go to transactions"
+        className="absolute text-palette-250 hover:text-palette-100 transition left-2 top-2"
+      >
+        <ArrowUpLeft className="size-4" />
+      </Link>
+      <h2 className="text-right w-full text-palette-100 text-xl font-medium">
         Últimas 5 transacciones
       </h2>
       <ul className="size-full flex flex-col">
