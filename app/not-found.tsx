c8b17b6e7b1bed856b2360c506f0d0cfd969d6cd
Julian Prieto <prietojulian2003@gmail.com>
2024-05-18 15:12:14 -0300

    add not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { WindowXmark } from "iconoir-react";
+
+export default function NotFound() {
+  return (
+    <main className="pt-12 pb-6 px-10 grid place-content-center bg-palette-400 text-palette-100 size-full">
+      <h2 className="flex flex-row items-center gap-8 text-7xl text-center font-semibold text-palette-500">
+        <WindowXmark />
+        404: Not found
+      </h2>
+      <Link
+        href="/"
+        className="text-2xl text-center text-palette-250 hover:text-palette-100 hover:underline transition"
+      >
+        Volver al Inicio
+      </Link>
+    </main>
+  );
+}
