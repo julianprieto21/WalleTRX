b73f6bbff872d534b10847a1254470fc096c1b16
Julian Prieto <prietojulian2003@gmail.com>
2024-06-05 22:29:48 -0300

    change a element to Link

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import { NavLinks, NavLinksMobile } from "./NavLinks";
 import { WalletSolid } from "iconoir-react";
 import { dict } from "@lib/dictionaries";
+import Link from "next/link";
 
 export function SideNav() {
   const { nav } = dict;
@@ -9,17 +10,17 @@ export function SideNav() {
     <>
       <aside className="relative hidden 2xl:flex w-1/6 flex-none flex-col items-center justify-center bg-palette-300 h-screen pb-2 overflow-auto">
         <section className="w-full flex flex-col items-center text-neutral-200 bg-palette-300 py-3 border-b border-palette-250">
-          <a href="/" title="Home">
+          <Link href="/" title="Home">
             <WalletSolid className="size-36 text-palette-100" />
-          </a>
-          <a href="/" title="Home">
+          </Link>
+          <Link href="/" title="Home">
             <h1 className="flex flex-row justify-center font-light text-5xl text-palette-100">
               {title[0]}
               <strong className="font-semibold text-palette-500">
                 {title[1]}
               </strong>
             </h1>
-          </a>
+          </Link>
         </section>
         <div className="flex flex-col w-full h-[90%] justify-start gap-4 py-4">
           <NavLinks />
@@ -30,9 +31,9 @@ export function SideNav() {
           </h1>
         </footer>
       </aside>
-      <nav className="2xl:hidden fixed bottom-0 w-full h-[72px] hidden flex-row border-t border-palette-250 bg-palette-400">
+      {/* <nav className="2xl:hidden fixed bottom-0 w-full h-[72px] hidden flex-row border-t border-palette-250 bg-palette-400">
         <NavLinksMobile />
-      </nav>
+      </nav> */}
     </>
   );
 }
