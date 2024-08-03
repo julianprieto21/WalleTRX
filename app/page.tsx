460a3faae63aeeb38dcdeff5b5122e3542d24ee8
Julian Prieto <prietojulian2003@gmail.com>
2024-08-03 20:05:28 -0300

    fix gap for small screens

--- app/page.tsx
+++ app/page.tsx
@@ -14,17 +14,19 @@ export default async function HomePage() {
   const user = await getUser();
   if (!user) return;
   return (
-    <main className="page pt-8 2xl:pt-12 pb-6 px-4 2xl:px-10">
-      <h2 className="font-semibold text-5xl text-palette-100 mt-4">
-        {helloText},{" "}
-        <span className="text-palette-500">{user.name.split(" ")[0]}</span>!
-      </h2>
-      <Greetings />
+    <main className="page pt-8 2xl:pt-12 pb-6 px-4 2xl:px-10 gap-8">
+      <div className=" mt-4">
+        <span className="font-semibold text-5xl text-palette-100">
+          {helloText},{" "}
+          <span className="text-palette-500">{user.name.split(" ")[0]}!</span>
+        </span>
+        <Greetings />
+      </div>
 
-      <div className="mt-10 2xl:mt-0 h-32 2xl:h-auto p-2 2xl:absolute 2xl:right-10 2xl:top-10 overflow-x-scroll overflow-y-hidden no-scrollbar">
+      <div className="mt-0 2xl:mt-0 h-32 2xl:h-auto p-2 2xl:absolute 2xl:right-10 2xl:top-10 overflow-x-scroll overflow-y-hidden no-scrollbar">
         <Accounts />
       </div>
-      <div className="w-full h-full 2xl:h-auto flex flex-col items-center 2xl:flex-row gap-0 2xl:gap-8 mt-12">
+      <div className="w-full h-full 2xl:h-auto flex flex-col items-center 2xl:flex-row gap-0 2xl:gap-8 mt-0">
         <Suspense
           fallback={
             <div className=" shrink-0 rounded-lg h-80 w-[620px] shadow-md bg-palette-300 px-6 py-5"></div>
